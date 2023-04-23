79f0f34080da86e254a6bb1aa04a62c3cacd773d
mo <mo3789530@gmail.com>
2023-04-23 21:57:28 +0900

    use helmet

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import helmet from "helmet";
 
 const app: express.Express = express();
 const port = 8000;
+
+app.use(helmet());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello, world!");
